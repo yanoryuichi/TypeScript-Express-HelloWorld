82c537b62e0697df8ab727e0f118ad9a9f3b55fd
Yano <yano@yanoryuichi.com>
2019-04-23 18:18:48 +0900

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+    target: 'node',
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src/app.js'),
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    output: {
+        filename: 'app.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
